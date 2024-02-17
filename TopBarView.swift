cee6508dbd08a13c8c3346936394de16747ebd46
Mihail <153065029+zuravlevmihail@users.noreply.github.com>
2024-02-17 11:38:07 +0100

    Update TopBarView.swift

--- TopBarView.swift
+++ TopBarView.swift
@@ -22,3 +22,27 @@ struct TopBarView: View {
             } label: {
                 Image(systemName: "square.and.arrow.down")
             }
+        }
+        .padding(.horizontal, 15)
+        .font(.title)
+        .frame(height: 50)
+        .background(.regularMaterial)
+        .cornerRadius(10)
+        .padding(.horizontal, 10)
+        .sheet(isPresented: $showSaveSheet) {
+            if let data {
+                DataExporterView(data: data, fileName: "Collage")
+            }
+        }
+        .sheet(isPresented: $showLoadSheet) {
+            DataImporterView(data: $data)
+        }
+        .onChange(of: data) { _ in
+            loadCollage()
+        }
+    }
+    
+    private func saveCollage() {
+        data = try? JSONEncoder().encode(store.state.collage)
+        showSaveSheet.toggle()
+    }
