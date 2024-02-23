62f7fd106af697a5aa323b4d92c7c035fb7cb09a
Mihail <153065029+zuravlevmihail@users.noreply.github.com>
2024-02-23 12:25:44 +0100

    Update TopBarView.swift

--- TopBarView.swift
+++ TopBarView.swift
@@ -46,3 +46,22 @@ struct TopBarView: View {
         data = try? JSONEncoder().encode(store.state.collage)
         showSaveSheet.toggle()
     }
+    
+    private func loadCollage() {
+        guard let data,
+              let collage = try? JSONDecoder().decode(
+                Collage.self,
+                from: data
+              ) else {
+            return
+        }
+        
+        store.dispatch(.setCollage(collage))
+    }
+}
+
+struct TopBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopBarView()
+    }
+}
